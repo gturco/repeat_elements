5b48924ae682ced4295877c354ca4d825e254422
Gina Turco <gturco88@gmail.com>
2013-11-03 15:39:35 -0800

    cleaned up and added outfile beformat

--- find_repeats.py
+++ find_repeats.py
@@ -6,37 +6,27 @@ from collections import defaultdict
 ### find Ns in fastafile
 ### get diff using numpy between one fasta and other
 
-def find_Ns(fastafile):
+def find_repeats(fastafile,outfile):
     f = Fasta(fastafile)
     d = defaultdict(list)
+    out = open(outfile,"wb")
     for chrm in f:
         chrm_str = f[chrm][:]
-        #print chrm_str
         ### match as many repeats get as many as possible lower or upper
         #repeat_reagions = re.finditer("X{1,}",chrm_str, flags=re.IGNORECASE)
         repeat_reagions = re.finditer("[a-z]{1,}",chrm_str)
         rep_pos = [repeat.span() for repeat in repeat_reagions]
         d[chrm].append(rep_pos)
-        #return rep_pos
-        #pos = np.array(rep_pos, dtype = ('int16','int16'))
+        name = "{0}_{1}_{2}".format(chrm,rep_pos[0], rep_pos[1])
+        out.write("{0}\t{1}\t{2}\t{4}\t".format(chrm,rep_pos[0],rep_pos[1],name))
+    out.close()
     return d
 
-#first remove all ranges that are the same using intersect
-#sec remove all ranges within
-#(10-14)
-#if (10-14) in list (10-21),(11,22),(12,40)
-#[fun(s,e) for s,e in cord]
-#map()
-#function(s,e):
-#    for s,e
-#    start looping based on 10 int
-#    10 in range new end = 15-21
-
-#find_Ns("sorgtest.fasta")
 if __name__ == '__main__':
     import optparse
     parser = optparse.OptionParser("usage: %prog [options] ")
     parser.add_option("--RMfasta", dest="fastafile", help="repeatmask fasta file")
+    parser.add_option("--out", dest="outfile", help="repeatmask fasta file")
     (options, _) = parser.parse_args()
 
-    find_Ns(options.fastafile)
+    find_repeats(options.fastafile, options.outfile)
